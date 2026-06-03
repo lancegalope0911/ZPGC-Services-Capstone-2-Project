3d9d59eddb35e7ec878c5cbee14e6786e4693add
lancegalope0911 <lancegalope0911@gmail.com>
2026-06-03 09:43:23 +0800

    Add files via upload

---
+++ CP2/pages/landing_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Linking to Stylesheet-->
+    <link rel="stylesheet" href="../css/landing_page.css">
+    <title>ZPGC Services</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="navdiv">
+            <div class="logo">
+                <a href="#">
+                    <img src="../images/ZPGC.com.png" alt="ZPGC">
+                </a>
+            </div>
+            <ul>
+                <button class="btnW"><a href="../pages/login_signup.php">Login</a></button>
+            </ul>
+        </div>
+    </nav>
+    <div class="hero">
+        <h1 id="header">Welcome to ZPGC Services!</h1>
+        <br>
+        <h4 id="subhead">Experience seamless technology support that prioritizes your workflow and minimizes downtime.
+        </h4>
+        <br>
+        <button class="btnR"><a href="../pages/login_signup.php">Sign up now!</a></button>
+    </div>
+</body>
+
+</html>
